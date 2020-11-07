5e757b802e66f74a4f3db487eb53f6d24c14e002
Cooper Saye <coopersaye@gmail.com>
2020-11-07 13:21:36 -0800

    Implement basic LeanTween animation.

---
+++ Assets/LeanTweenTesting/Scripts/Logo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace LeanTweenTesting
+{
+    public class Logo : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) LeanTween.move(gameObject, transform.position + Vector3.one, 1);
+        }
+    }
+}
